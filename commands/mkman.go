286caf0fd1ecfcb13d0da67c35633f64a25536fe
Nima Kaviani <nkavian@us.ibm.com>
2016-03-21 10:43:23 -0700

    Remove print-amit command from mkman
    [#115983367]
    
    Signed-off-by: Alvaro Perez-Shirley <apshirley@pivotal.io>

--- commands/mkman.go
+++ commands/mkman.go
@@ -2,7 +2,6 @@ package commands
 
 type MkmanCommand struct {
 	Version         func()                 `long:"version" description:"Print version"`
-	PrintAmit       PrintAmitCommand       `command:"print-amit" description:"Prints the man behind 'mkman'"`
 	CreateManifests CreateManifestsCommand `command:"create-manifests" description:"Make a manifest from a config file"`
 }
 
